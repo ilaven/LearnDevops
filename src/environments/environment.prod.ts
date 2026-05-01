1116c4f48aff0064d78ad6ba42d76e34a4d9c3df
ilaven <ilavenj23@gmail.com>
2026-05-01 11:31:17 +0530

    removed config files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,17 +10,9 @@ export const environment = {
     messagingSenderId: '',
     appId: '',
     measurementId: ''
-  },
-  // urlPath:"https://portal.myljm.com/Ljmweb/",
-  // imageURL: "https://portal.myljm.com/Ljm_image/",
+  }, 
   urlPath: "http://162.242.196.195:92/Ljmweb/",
-  imageURL: "http://162.242.196.195:92/Ljm_image/",
-  // urlPath:"http://192.168.1.202:8080/Ljmweb/",
-  // imageURL:"http://192.168.1.202:8080/Ljm_image/",
-  // crmUrl: "https://crm.myljm.com/WebAPI/",
+  imageURL: "http://162.242.196.195:92/Ljm_image/", 
   crmUrl: "http://162.242.196.194:85/WebAPI/",
-  crmUrlnew: "http://162.242.196.194:85/CRMWKHIVE/",
-  // crmUrl: "http://162.242.196.193/WebAPI/",
-  //urlPath:"http://20.1.1.123:8080/Ljmweb/"
-  // urlPath:"http://localhost:8080/Ljmweb/"
+  crmUrlnew: "http://162.242.196.194:85/CRMWKHIVE/", 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -15,18 +15,10 @@ export const environment = {
     appId: '',
     measurementId: ''
   },
-  // urlPath:"https://portal.myljm.com/Ljmweb/",
-  // imageURL: "https://portal.myljm.com/Ljm_image/",
   urlPath:"http://162.242.196.195:92/Ljmweb/",
   imageURL:"http://162.242.196.195:92/Ljm_image/",
-  // urlPath:"http://192.168.1.202:8080/Ljmweb/",
-  // imageURL:"http://192.168.1.202:8080/Ljm_image/",
-  // crmUrl: "https://crm.myljm.com/WebAPI/",
   crmUrl: "http://162.242.196.194:85/WebAPI/",
   crmUrlnew : "http://162.242.196.194:85/CRMWKHIVE/",
-  // crmUrl: "http://162.242.196.193/WebAPI/",
-  //urlPath:"http://20.1.1.123:8080/Ljmweb/"
-  // urlPath:"http://localhost:8080/Ljmweb/"
 };
  
 /*
